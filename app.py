edb6af5102f692cd1cff2626026217515c9a9ecd
frank-xw <24924150+frank-xw@users.noreply.github.com>
2023-08-06 18:52:02 -0400

    Add reading pdf
    Revert it to read single PDF functionality

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import streamlit as st
-
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader # take text from PDF
 
 def main():
-    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    load_dotenv()
 
-    st.header("Chat with multiple PDFs :books:")
+
+    # streamlit 
+    st.set_page_config(page_title="Chat with your PDF", page_icon=":books:")
+    st.header("Ask your PDF 💬")
     st.text_input("Ask a questions about your documents:")
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        st.file_uploader(label="Upload your PDFs here and click on 'Process'")
-        st.button("Process")
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        st.write(text)
+
+
+    # with st.sidebar:
+    #     st.subheader("Your documents")
+    #     st.file_uploader(label="Upload your PDFs here and click on 'Process'")
+    #     st.button("Process")
 
 
 
